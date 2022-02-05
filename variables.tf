1a70070cb3a49b7b1c6bc8288eb5f4857de9bc0a
labdemo2233 <91132239+labdemo2233@users.noreply.github.com>
2022-02-06 10:23:17 +1100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,37 @@
+variable "agent_count" {
+    default = 2
+}
+
+variable "ssh_public_key" {
+    default = "~/.ssh/id_rsa.pub"
+}
+
+variable "dns_prefix" {
+    default = "k8stest"
+}
+
+variable cluster_name {
+    default = "k8stest"
+}
+
+variable resource_group_name {
+    default = "azure-k8stest"
+}
+
+variable location {
+    default = "Australia East"
+}
+
+variable log_analytics_workspace_name {
+    default = "testLogAnalyticsWorkspaceName"
+}
+
+# refer https://azure.microsoft.com/global-infrastructure/services/?products=monitor for log analytics available regions
+variable log_analytics_workspace_location {
+    default = "australiaeast"
+}
+
+# refer https://azure.microsoft.com/pricing/details/monitor/ for log analytics pricing 
+variable log_analytics_workspace_sku {
+    default = "PerGB2018"
+}
