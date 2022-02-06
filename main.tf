7dac10b1597147c1211d8c35206ea244cfc2b519
labdemo2233 <91132239+labdemo2233@users.noreply.github.com>
2022-02-06 22:23:35 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,14 +19,14 @@ data "azurerm_key_vault_secret" "spn_secret" {
 
 resource "azurerm_virtual_network" "aks_vnet" {
   name                = var.aks_vnet_name
-  resource_group_name = azurerm_resource_group.aks_demo_rg.name
-  location            = azurerm_resource_group.aks_demo_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  location            = azurerm_resource_group.aks_rg.location
   address_space       = var.vnetcidr
 } 
 
 resource "azurerm_subnet" "aks_subnet" {
   name                 = "aks_subnet"
-  resource_group_name  = azurerm_resource_group.aks_demo_rg.name
+  resource_group_name  = azurerm_resource_group.aks_rg.name
   virtual_network_name = azurerm_virtual_network.aks_vnet.name
   address_prefixes       = var.subnetcidr
 }
