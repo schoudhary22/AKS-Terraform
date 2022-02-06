3cc8f575e07c6034d4cf761d886f54923b0aac4e
labdemo2233 <91132239+labdemo2233@users.noreply.github.com>
2022-02-06 21:49:07 +1100

    Create kube-prep-steps.ps1

---
+++ kube-prep-steps.ps1
@@ -0,0 +1,36 @@
+#### Parameters
+
+$keyvaultname = "xxxxx"
+$sshkeysecret = "yyyy"
+$spnclientid = "zzzz"
+$clientidkvsecretname = "cccc"
+$spnclientsecret = "ssss"
+$spnkvsecretname = "kkkk"
+
+
+#create an ssh key for setting up password-less login between agent nodes.
+
+ssh-keygen  -f ~/.ssh/id_rsa_terraform
+
+
+#### Add SSH Key in Azure Key vault secret
+
+$pubkey = cat ~/.ssh/id_rsa_terraform.pub
+
+$Secret = ConvertTo-SecureString -String $pubkey -AsPlainText -Force
+
+Set-AzKeyVaultSecret -VaultName $keyvaultname -Name $sshkeysecret -SecretValue $Secret
+
+
+#### Store service principal Client id in Azure KeyVault
+
+$Secret = ConvertTo-SecureString -String $spnclientid -AsPlainText -Force
+
+Set-AzKeyVaultSecret -VaultName $keyvaultname -Name $clientidkvsecretname -SecretValue $Secret
+
+
+#### Store service principal Secret in Azure KeyVault
+
+$Secret = ConvertTo-SecureString -String $spnclientsecret -AsPlainText -Force
+
+Set-AzKeyVaultSecret -VaultName $keyvaultname -Name $spnkvsecretname -SecretValue $Secret
