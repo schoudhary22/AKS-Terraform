356c30d0228a4f2fd2012224a1d88a28143b599a
labdemo2233 <91132239+labdemo2233@users.noreply.github.com>
2022-02-06 11:34:57 +1100

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,28 +1,7 @@
-# output "client_key" {
-#     value = azurerm_kubernetes_cluster.k8s.kube_config.0.client_key
-# }
-
-# output "client_certificate" {
-#     value = azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate
-# }
-
-output "cluster_ca_certificate" {
-    value = azurerm_kubernetes_cluster.k8s.kube_config.0.cluster_ca_certificate
-}
-
-output "cluster_username" {
-    value = azurerm_kubernetes_cluster.k8s.kube_config.0.username
-}
-
-output "cluster_password" {
-    value = azurerm_kubernetes_cluster.k8s.kube_config.0.password
+output "client_certificate" {
+  value = "${azurerm_kubernetes_cluster.aks_k2.kube_config.0.client_certificate}"
 }
 
 output "kube_config" {
-    value = azurerm_kubernetes_cluster.k8s.kube_config_raw
-    sensitive = true
-}
-
-output "host" {
-    value = azurerm_kubernetes_cluster.k8s.kube_config.0.host
+  value = "${azurerm_kubernetes_cluster.aks_k2.kube_config_raw}"
 }
