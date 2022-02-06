90c9118dce915fd36d60fe76b38df3a735a2a10f
labdemo2233 <91132239+labdemo2233@users.noreply.github.com>
2022-02-06 12:41:47 +1100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -49,11 +49,17 @@ variable "kubernetes_version" {
 
 #AKS Agent pools
 variable "agent_pools" {
+  type = object({
+      name            = string
+      count           = number
+      vm_size         = string
+      os_disk_size_gb = string
+    }
+  )
   default = {
       name            = "pool1"
       count           = 2
       vm_size         = "Standard_D2_v2"
-      os_type         = "Linux"
       os_disk_size_gb = "30"
     }
 }
