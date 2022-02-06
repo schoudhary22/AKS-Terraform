c6c964fabe70755857c9a3a4ecb928ea49784725
labdemo2233 <91132239+labdemo2233@users.noreply.github.com>
2022-02-07 02:04:36 +1100

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,8 +1,8 @@
 output "client_certificate" {
-  value = "${azurerm_kubernetes_cluster.aks_k2.kube_config.0.client_certificate}"
+  value = "${azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate}"
 }
 
 output "kube_config" {
-  value = "${azurerm_kubernetes_cluster.aks_k2.kube_config_raw}"
+  value = "${azurerm_kubernetes_cluster.aks_cluster.kube_config_raw}"
   sensitive = true
 }
