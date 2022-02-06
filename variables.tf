a614b7e6fc5cb96e4db447e4d13715e2c026aa71
labdemo2233 <91132239+labdemo2233@users.noreply.github.com>
2022-02-06 12:47:02 +1100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,50 +1,41 @@
 variable "aks_vnet_name" {
     type = string
-    default = "aksvnet"
 }
 
 #KeyVault Resource Group and KeyVaultName
 variable "keyvault_rg" {
   type    = string
-  default = "Lab-RG"
 }
 variable "keyvault_name" {
   type    = string
-  default = "demo-azurekv"
 }
 
 variable "azure_region" {
   type    = string
-  default = "australiaeast"
 }
 
 #  Resource Group Name
 variable "resource_group" {
   type    = string
-  default = "AKSCluster-RG"
 }
 
 # AKS Cluster name
 variable "cluster_name" {
   type    = string
-  default = "AKSTerraform"
 }
 
 #AKS DNS name
 variable "dns_name" {
   type    = string
-  default = "AKSTerraform"
 }
 
 variable "admin_username" {
   type    = string
-  default = "aksadmin"
 }
 
 # Specify a valid kubernetes version
 variable "kubernetes_version" {
   type    = string
-  default = "1.21.7"
 }
 
 #AKS Agent pools
@@ -56,10 +47,4 @@ variable "agent_pools" {
       os_disk_size_gb = string
     }
   )
-  default = {
-      name            = "pool1"
-      count           = 2
-      vm_size         = "Standard_D2_v2"
-      os_disk_size_gb = "30"
-    }
 }
